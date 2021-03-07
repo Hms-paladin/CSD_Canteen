a70a810b1ef7398c3368245da601b8bcf96c1d7c
Asraf <mohammedasraf11z@gmail.com>
2021-03-07 21:51:53 +0530

    sidebar active color

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link,useHistory} from "react-router-dom";
+import {Link,NavLink,useHistory} from "react-router-dom";
 import "./Sidebar.css";
 
 
@@ -12,48 +12,16 @@ const Sidebar = () => {
     return(
         <div className="sidebar"> 
             <ul>
-                <li>
-                <Link to="/dashboard">Dashboard</Link>
-                </li> 
-
-                <li>
-                <Link to="/orders">Orders</Link>
-                </li>
-       
-               <li>
-                <Link to="/picklist">Pick List Print</Link>
-                </li>
-
-                <li>
-                <Link to="/payment">Payment Details</Link>
-           
-               </li>
-
-               <li>
-                <Link to="/inventory">Inventory Upload</Link>
-                </li>
-
-              <li>
-                <Link to="/productmanagement">Product</Link>
-                </li>
-
-                <li>
-                <Link to="/newarrival">New Arrival</Link>
-               </li>
-
-                <li>
-                    <Link to="/pages">Pages</Link>
-                </li>
-
-                <li>
-                    <Link to="/career">Career</Link>
-                </li>
-
-                <li>
-                    <Link to="/instructionmanual">Instruction Manual</Link>
-                </li>
-
-             
+               <li> <NavLink  className="link-bef" activeClassName="active-link" to="/dashboard">Dashboard</NavLink></li>
+               <li><NavLink className="link-bef" activeClassName="active-link" to="/orders">Orders</NavLink></li>
+               <li><NavLink className="link-bef" activeClassName="active-link" to="/picklist">Pick List Print</NavLink></li>
+               <li><NavLink className="link-bef" activeClassName="active-link" to="/payment">Payment Details</NavLink></li>
+               <li><NavLink className="link-bef" activeClassName="active-link"  to="/inventory">Inventory Upload</NavLink></li>
+               <li><NavLink className="link-bef" activeClassName="active-link" to="/productmanagement">Product</NavLink></li>
+                <li> <NavLink className="link-bef" activeClassName="active-link" to="/newarrival">New Arrival</NavLink></li>
+                <li> <NavLink className="link-bef" activeClassName="active-link"  to="/pages">Pages</NavLink></li>
+                <li>    <NavLink className="link-bef" activeClassName="active-link" to="/career">Career</NavLink></li>
+                <li>  <NavLink className="link-bef" activeClassName="active-link" to="/instructionmanual">Instruction Manual</NavLink></li>
                 </ul>
            
         </div>
